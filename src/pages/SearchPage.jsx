3c6200cd8ff55469d8b2f48ad8d72d2eaac54713
Andi Nurul Atika <andinurulcs21@gmail.com>
2026-06-12 22:48:33 +0800

    Perbaiki tampilan responsif SearchPage di mobile

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -299,7 +299,7 @@ export default function SearchPage() {
               position: "relative",
               boxShadow: "0 10px 30px rgba(0,0,0,0.25)",
               backdropFilter: "blur(15px)",
-              zIndex: 10,
+              zIndex: 100,
             }}
           >
             
